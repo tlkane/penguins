bdbacbf27db475f30127ed15438f63cc15d03c11
Taylor <tlkane@alaska.edu>
2025-06-04 18:28:02 +0000

    Added code for a scatter plot to: my_code.R

--- my_code.R
+++ my_code.R
@@ -1,1 +1,3 @@
 # This is my first line of code
+ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g, color = species)) + geom_point()
+ +labs(x = "Flipper Length (mm)", y = "Body Mass (g)", color = "Species")
