40594085e14541561559b00c56d129cbe507ba6c
taylorkane <tlkane@alaska.edu>
2025-06-04 19:26:31 +0000

    Added data to R and plotted

--- my_code.R
+++ my_code.R
@@ -1,3 +1,6 @@
 # This is my first line of code
 ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g, color = species)) + geom_point()
  +labs(x = "Flipper Length (mm)", y = "Body Mass (g)", color = "Species")
+
+library(ggplot2)
+library(palmerpenguins)
